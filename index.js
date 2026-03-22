e1a9655e983fdf902231ba01ddefd121b8fc87cb
Vikram Thakur <vikramkk380@gmail.com>
2026-03-23 00:21:33 +0530

    socket connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
 import dotenv from 'dotenv'
+import cors from "cors"
 
 
 dotenv.config()
@@ -9,7 +10,20 @@ dotenv.config()
 const app = express()
 const server = http.createServer(app)
 
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+
 
 app.get("/", (req, res) => {
     res.json("hello Vikram Kumar Thakur")
