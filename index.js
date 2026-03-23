b734fa957905421d8d92a08083325937c7818ea4
Vikram Thakur <vikramkk380@gmail.com>
2026-03-23 21:10:42 +0530

    user tracing

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ import dotenv from 'dotenv'
 import cors from "cors"
 
 
+
 dotenv.config()
 
 const app = express()
+app.use(express.json())
 const server = http.createServer(app)
+
+
+let onilineUsers = {}
 
 const io = new Server(server, {
     cors: {
@@ -16,9 +21,27 @@ const io = new Server(server, {
     }
 });
 io.on("connection", (socket) => {
+    socket.on("addUser", (userId) => {
+        onilineUsers[userId] = socket.id
+        console.log("online Users: ", onilineUsers)
+
+        io.emit("getOnlineUsers", Object.keys(onilineUsers))
+    })
     console.log("User connected:", socket.id);
 
+    socket.on("sendMessage", (data) => {
+        console.log("Message received:", data);
+
+        io.emit("receiveMessage", data);
+    });
+
     socket.on("disconnect", () => {
+        for (let userId in onilineUsers) {
+            if (onilineUsers[userId] === socket.id) {
+                delete (onilineUsers[userId])
+            }
+        }
+        io.emit("getOnlineUsers", Object.keys(onilineUsers))
         console.log("User disconnected");
     });
 });
